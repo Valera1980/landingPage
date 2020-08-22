3d4688487de678666e56604fee60a0eb0e8db714
Valera <gavrilow777@gmail.com>
2020-08-22 12:27:04 +0300

    form completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormOneComponent } from './form-one/form-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormOneComponent
   ],
   imports: [
     BrowserModule,
